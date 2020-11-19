8b9b623dc3c00abf715bd571b4f902d6b4aee07c
payyd <47188226+payyd@users.noreply.github.com>
2020-11-19 20:23:34 +0000

    Add files via upload
    Initial setup

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main
+{	
+
+	public void startUI()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new UI());
+		
+	}
+
+	public static void main(String[] args)
+	{
+		Main main = new Main();
+		main.startUI();			
+	}
+}
